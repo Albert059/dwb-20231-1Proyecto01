d07c256116c791ac1453aaed5974ecdb6a9982d1
Albert059 <albert059@ciencias.unam.mx>
2022-12-02 03:56:07 -0600

    Arreglado invoice

--- invoice/src/main/java/com/invoice/configuration/client/ProductClient.java
+++ invoice/src/main/java/com/invoice/configuration/client/ProductClient.java
@@ -1,5 +1,6 @@
 package com.invoice.configuration.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,8 @@ import com.invoice.api.dto.ApiResponse;
 import com.invoice.api.dto.DtoProduct;
 
 
+
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
 	/*
@@ -16,9 +19,8 @@ public interface ProductClient {
 	 * Actualizar método getProduct para obtener la información necesaria de un producto
 	 */
 	
-	
 	@GetMapping("product/{gtin}")
-	public ResponseEntity<DtoProduct> getProduct(@PathVariable("gtin") String gtin);
+	ResponseEntity<DtoProduct> getProduct(@PathVariable("gtin") String gtin);
 	
 	@PutMapping("product/{gtin}/stock/{stock}")
 	public ResponseEntity<ApiResponse> updateProductStock(@PathVariable("gtin") String gtin, @PathVariable("stock") Integer stock);
